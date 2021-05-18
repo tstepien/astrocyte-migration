a6c8a0229d23f0b4b3ded6fbd4606e09f5f5df34
Tracy Stepien <tstepien@ufl.edu>
2021-05-18 14:38:35 -0400

    rearrange files

--- old code moved aug 17/growthterms_sum.m
+++
@@ -1,9 +0,0 @@
-function g = growthterms_sum(c1,c2,PO2,Pm,alpha1,alpha2,gamma1,gamma2,rbar)
-% g = growthterms_sum(c1,c2,PO2,Pm,alpha1,alpha2,beta,gamma1,gamma2,rbar)
-%
-% growth terms
-
-cmax = 1/(pi*rbar^2);
-
-g = PO2./(Pm+PO2).*(alpha1*c1 + alpha2*c2).*(1 - (c1+c2)/cmax) ...
-    - (gamma1*c1 + gamma2*c2);
