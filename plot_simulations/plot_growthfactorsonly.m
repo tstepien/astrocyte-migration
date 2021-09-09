27521a5164bf00484e1deb9162151d19cb90acc4
Tracy Stepien <tstepien@ufl.edu>
2021-09-08 22:08:59 -0400

    update plots

--- plot_simulations/plot_growthfactorsonly.m
+++ plot_simulations/plot_growthfactorsonly.m
@@ -18,13 +18,17 @@ co = [0    0.4470    0.7410
     0.6350    0.0780    0.1840
     0         0.3725    0];
 
-fslabel = 22;
-fsticks = 18;
-flegend = 14;
-fstitles = 24;
+fstitle = 26;
+fslabel = 26;
+fsticks = 22;
+fslegend = 18;
 
 % set up radius
-[~,~,radius_endo,radius_ret] = thick_rad(t,r);
+radius_endo = zeros(length(t),1);
+radius_ret = zeros(length(t),1);
+for i=1:length(t)
+    [~,~,radius_endo(i),radius_ret(i)] = thick_rad(t(i),r);
+end
 
 % set up PDGFA and LIF
 r = zeros(numcurvesplot,nxpts+1);
@@ -42,13 +46,14 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if strcmp(subpanels_on,'yes')==1
     figure
-    subaxis(2,2,1,'MarginLeft',0.07,'MarginRight',0.05,'MarginTop',0.04,'MarginBottom',0.12)
+    tiledlayout(2,2,'TileSpacing','compact','Padding','none')
+    nexttile
 else
     figure
 end
 hold on
 for i=1:numcurvesplot
-   plot(r(i,:),y1(i,:),'LineWidth',1.5,'Color',co(i,:)) 
+   plot(r(i,:),y1(i,:),'LineWidth',2.5,'Color',co(i,:)) 
 end
 hold off
 xlabel('$r$ (mm)','FontSize',fslabel,'Interpreter','latex')
@@ -56,31 +61,31 @@ ylabel('PDGFA (ng/mL)','FontSize',fslabel,'Interpreter','latex')
 box off
 
 h = legend('E15','E16','E17','E18','E19','E20','E21','E22/P0');
-set(h,'FontSize',flegend);
+set(h,'FontSize',fslegend,'Position',[0.813 0.447 0.181 0.434]);
 
 if strcmp(subpanels_on,'yes')==0
-    set(gca,'XLim',[0,rmax],'FontSize',fsticks,'Position',[0.14 0.14 0.82 0.76])
+    set(gca,'XLim',[0,rmax],'FontSize',fsticks,'Position',[0.175 0.14 0.79 0.76])
     set(gcf,'Units','inches','Position',[2 2 7.75 5.75],'PaperPositionMode','auto')
 else
     set(gca,'XLim',[0,rmax],'FontSize',fsticks)
 end
 
 if strcmp(titles_on,'yes')==1
-    title('A                                                    ',...
-    'FontSize',fstitles)
+    title('A                                             ',...
+    'FontSize',fstitle)
 end
 
 pause(0.1)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if strcmp(subpanels_on,'yes')==1
-    subaxis(2,2,2,'MarginRight',0.01,'MarginLeft',0.07)
+    nexttile
 else
     figure
 end
 hold on
 for i=1:numcurvesplot
-   plot(r(i,:),y2(i,:),'LineWidth',1.5,'Color',co(i,:)) 
+   plot(r(i,:),y2(i,:),'LineWidth',2.5,'Color',co(i,:)) 
 end
 hold off
 xlabel('$r$ (mm)','FontSize',fslabel,'Interpreter','latex')
@@ -88,30 +93,30 @@ ylabel('LIF (ng/mL)','FontSize',fslabel,'Interpreter','latex')
 box off
 
 h = legend('E15','E16','E17','E18','E19','E20','E21','E22/P0');
-set(h,'FontSize',flegend);
+set(h,'FontSize',fslegend);
 
 if strcmp(subpanels_on,'yes')==0
-    set(gca,'XLim',[0,rmax],'FontSize',fsticks,'Position',[0.14 0.14 0.82 0.76])
+    set(gca,'XLim',[0,rmax],'FontSize',fsticks,'Position',[0.175 0.14 0.79 0.76])
     set(gcf,'Units','inches','Position',[2 2 7.75 5.75],'PaperPositionMode','auto')
 else
     set(gca,'XLim',[0,rmax],'FontSize',fsticks)
 end
 
 if strcmp(titles_on,'yes')==1
-    title('B                                                    ',...
-    'FontSize',fstitles)
+    title('B                                             ',...
+    'FontSize',fstitle)
 end
 
 pause(0.1)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if strcmp(subpanels_on,'yes')==1
-    subaxis(2,2,3,'MarginLeft',0.07,'MarginRight',0.05,'MarginBottom',0.08,'MarginTop',0.08)
+    nexttile
 else
     figure
 end
 hold on
-plot(radius_ret(tplot),t(tplot)/24,'k','LineWidth',1.5)
+plot(radius_ret(tplot),t(tplot)/24,'k','LineWidth',2.5)
 scatter(radius_ret(tplot),t(tplot)/24,70,co,'filled')
 hold off
 ylabel('$t$ (days)','FontSize',fslabel,'Interpreter','latex')
@@ -120,27 +125,27 @@ yticks(0:7)
 box off
 
 if strcmp(subpanels_on,'yes')==0
-    set(gca,'XLim',[0,rmax],'FontSize',fsticks,'Position',[0.14 0.14 0.82 0.76])
+    set(gca,'XLim',[0,rmax],'YLim',[0,7],'FontSize',fsticks,'Position',[0.175 0.14 0.79 0.76])
     set(gcf,'Units','inches','Position',[2 2 7.75 5.75],'PaperPositionMode','auto')
 else
     set(gca,'XLim',[0,rmax],'YLim',[0,7],'FontSize',fsticks)
 end
 
 if strcmp(titles_on,'yes')==1
-    title('C                                                    ',...
-    'FontSize',fstitles)
+    title('C                                             ',...
+    'FontSize',fstitle)
 end
 
 pause(0.1)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if strcmp(subpanels_on,'yes')==1
-    subaxis(2,2,4,'MarginRight',0.01,'MarginLeft',0.07)
+    nexttile
 else
     figure
 end
 hold on
-plot(radius_endo(tplot),t(tplot)/24,'k','LineWidth',1.5)
+plot(radius_endo(tplot),t(tplot)/24,'k','LineWidth',2.5)
 scatter(radius_endo(tplot),t(tplot)/24,70,co,'filled')
 hold off
 ylabel('$t$ (days)','FontSize',fslabel,'Interpreter','latex')
@@ -149,15 +154,15 @@ yticks(0:7)
 box off
 
 if strcmp(subpanels_on,'yes')==0
-    set(gca,'XLim',[0,rmax],'FontSize',fsticks,'Position',[0.14 0.14 0.82 0.76])
+    set(gca,'XLim',[0,rmax],'YLim',[0,7],'FontSize',fsticks,'Position',[0.175 0.14 0.79 0.76])
     set(gcf,'Units','inches','Position',[2 2 7.75 5.75],'PaperPositionMode','auto')
 else
     set(gca,'XLim',[0,rmax],'YLim',[0,7],'FontSize',fsticks)
 end
 
 if strcmp(titles_on,'yes')==1
-    title('D                                                    ',...
-    'FontSize',fstitles)
+    title('D                                             ',...
+    'FontSize',fstitle)
 end
 
 pause(0.1)
